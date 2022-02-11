6706d595a19c973538307d665f2b88fbf03da33d
Honma Ryota <79443099+HonmaRyota@users.noreply.github.com>
2022-02-11 22:39:16 +0900

    memoによる最適化

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {useState} from "react";
+import {ChildArea} from "./ChildArea.jsx";
+import "./App.css";
+
+export default function App() {
+  console.log("app");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (event) => setText(event.target.value);
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </div>
+  );
+}
