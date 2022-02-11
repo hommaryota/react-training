a278ae526c7cc2407872b4b9c27cd3d77ffb4bd4
Honma Ryota <79443099+HonmaRyota@users.noreply.github.com>
2022-02-11 23:19:26 +0900

    useCallbackによる最適化

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useCallback, useMemo} from "react";
 import {ChildArea} from "./ChildArea.jsx";
 import "./App.css";
 
@@ -9,17 +9,22 @@ export default function App() {
 
   const onChangeText = (event) => setText(event.target.value);
 
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
+  const onClickOpen = () => setOpen(!open);
 
+  // アロー関数で記述すると、毎回新しい関数が再生成されて呼び出されていると判断される
+  // そのため、useCallbackで囲む
+  // 囲まれた関数は、第二引数のみを管理する
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
